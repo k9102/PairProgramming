a5f511a096c3d02552d0dd04bd82e0def8dafefa
k9102 <k9102@k9102ui-MacBook-Pro.local>
2018-09-15 17:12:16 +0900

    class version

--- PairProgramming.cpp
+++ PairProgramming.cpp
@@ -12,66 +12,103 @@ using namespace std;
 
 #define EMPTY -1
 #define OBS -2
-#define MAXDIR 4
+#define MAXTRY 4
 
 enum
 {
     RIGHT = 0,
     DOWN,
     LEFT,
-    UP
+    UP,
+    MAXDIR
 };
 
-int cur_val = 0;
-void traverse(vector<vector<int>> &maze, int cur_x, int cur_y, int cur_dir, int try_num)
+class SpiralMatrix
 {
-    int row = maze.size();
-    int col = maze[0].size();
+  private:
+    int cur_val_ = 0;
+    int col_, row_;
+    vector<vector<int>> maze_;
 
-    if(MAXDIR <= try_num) return;
+  public:
+    SpiralMatrix(int col, int row, const vector<pair<int, int>> &obstacle) : maze_(row, vector<int>(col, EMPTY)),
+                                                                             col_(col), row_(row)
+    {
+        for (auto [x, y] : obstacle)
+        {
+            maze_[y][x] = OBS;
+        }
+    }
+    void Print()
+    {
+        for (int y = 0; y < row_; y++)
+        {
+            for (int x = 0; x < col_; x++)
+            {
+                int val = maze_[y][x];
+                if (val == EMPTY)
+                {
+                    cout << "\t"
+                         << "-";
+                }
+                else if (val == OBS)
+                {
+                    cout << "\t"
+                         << "x";
+                }
+                else
+                {
+                    cout << "\t" << val;
+                }
+            }
+            cout << "\n";
+        }
+    }
+    void Traverse(int cur_x, int cur_y, int cur_dir, int trial_num)
+    {
+        if (MAXTRY <= trial_num)
+            return;
 
-    int next_x = cur_x, next_y = cur_y;
-    switch (cur_dir)
-    {
-    case RIGHT:
-        next_x++;
-        break;
-    case DOWN:
-        next_y++;
-        break;
-    case LEFT:
-        next_x--;
-        break;
-    case UP:
-        next_y--;
-        break;
-    }
+        int next_x = cur_x, next_y = cur_y;
+        switch (cur_dir)
+        {
+        case RIGHT:
+            next_x++;
+            break;
+        case DOWN:
+            next_y++;
+            break;
+        case LEFT:
+            next_x--;
+            break;
+        case UP:
+            next_y--;
+            break;
+        }
 
-    if (
-        next_x < 0 || next_x >= col ||
-        next_y < 0 || next_y >= row ||
-        maze[next_y][next_x] != EMPTY)
-    {
- 
-        traverse(maze, cur_x, cur_y, (cur_dir + 1) % MAXDIR, try_num+1);
-    
+        if (
+            next_x < 0 || next_x >= col_ ||
+            next_y < 0 || next_y >= row_ ||
+            maze_[next_y][next_x] != EMPTY)
+        {
+
+            Traverse(cur_x, cur_y, (cur_dir + 1) % MAXDIR, trial_num + 1);
+        }
+        else
+        {
+            maze_[next_y][next_x] = cur_val_++;
+            Traverse(next_x, next_y, cur_dir, 0);
+        }
     }
-    else
-    {
-        maze[next_y][next_x] = cur_val++;
-        traverse(maze, next_x, next_y, cur_dir,0);
-    }
-}
+};
 
 int main()
 {
     int row, col;
     int obs_num;
+    vector<pair<int, int>> obstacle;
 
     cin >> col >> row;
-
-    vector<vector<int>> maze(row, vector<int>(col, EMPTY));
-
     cin >> obs_num;
 
     int x, y;
@@ -79,33 +116,14 @@ int main()
     {
         cin >> x >> y;
 
-        maze[y][x] = OBS;
+        obstacle.emplace_back(x, y);
     }
 
-    traverse(maze, -1, 0, RIGHT,0);
+    SpiralMatrix smat(col, row, obstacle);
+    smat.Print();
+    smat.Traverse(-1, 0, RIGHT, 0);
 
-    for (int y = 0; y < row; y++)
-    {
-        for (int x = 0; x < col; x++)
-        {
-            int val = maze[y][x];
-            if (val == EMPTY)
-            {
-                cout << "\t"
-                     << "-";
-            }
-            else if (val == OBS)
-            {
-                cout << "\t"
-                     << "x";
-            }
-            else
-            {
-                cout << "\t" << val;
-            }
-        }
-        cout << "\n";
-    }
+    smat.Print();
 
     return 0;
 }
